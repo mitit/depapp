06b9c9c03c1e44f01858fe5838d0cda3ab8d3a7a
dmitrysemenov <mitsem843@gmail.com>
2017-09-08 11:39:58 +0300

    creating xml file from db implemented

--- src/main/java/group/depapp/util/CreateXMLFile.java
+++ src/main/java/group/depapp/util/CreateXMLFile.java
@@ -2,6 +2,11 @@ package group.depapp.util;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import group.depapp.domain.DepartmentDTO;
 import group.depapp.service.DepartmentService;
 import group.depapp.service.DepartmentServiceImpl;
@@ -9,6 +14,8 @@ import org.springframework.stereotype.Component;
 import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+
+import java.io.File;
 import java.util.List;
 
 @Component
@@ -18,6 +25,10 @@ public class CreateXMLFile {
 
         DepartmentServiceImpl departmentService = new DepartmentServiceImpl();
         List<DepartmentDTO> departmentDTOList = departmentService.getAll();
+
+        for (DepartmentDTO departmentDTO : departmentDTOList)
+            System.out.println("c:" + departmentDTO.getDepCode() + " j:" +
+                    departmentDTO.getDepJob() + " desc:" + departmentDTO.getDescription());
 
         try {
             DocumentBuilderFactory dbFactory =
@@ -30,7 +41,8 @@ public class CreateXMLFile {
             doc.appendChild(rootElement);
 
             for (DepartmentDTO departmentDTO : departmentDTOList) {
-                Element department = doc.createElement("department");
+                Element department = doc.createElement("department" + departmentDTO.getDepCode() + departmentDTO.getDepJob());
+                rootElement.appendChild(department);
 //                Attr attrID = doc.createAttribute("id");
 //                attrID.setValue(departmentDTO.getId().toString());
 
@@ -42,12 +54,18 @@ public class CreateXMLFile {
                 depJob.appendChild(doc.createTextNode(departmentDTO.getDepJob()));
                 department.appendChild(depJob);
 
-                Element desc = doc.createElement("description");
-                desc.appendChild(doc.createTextNode(departmentDTO.getDescription()));
-                department.appendChild(desc);
-
-
+                if (departmentDTO.getDescription() != null) {
+                    Element desc = doc.createElement("description");
+                    desc.appendChild(doc.createTextNode(departmentDTO.getDescription()));
+                    department.appendChild(desc);
+                }
             }
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new File("db.xml"));
+            transformer.transform(source, result);
         } catch (Exception e) {
             e.printStackTrace();
         }
