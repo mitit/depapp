5f13fd8ac8f724df03cad431f7684e14dc3b7315
dmitrysemenov <mitsem843@gmail.com>
2017-09-09 13:59:38 +0300

    реализация выбора команды в DepAppApplication

--- src/main/java/group/depapp/DepappApplication.java
+++ src/main/java/group/depapp/DepappApplication.java
@@ -3,6 +3,7 @@ package group.depapp;
 
 import group.depapp.util.Synchronizer;
 import group.depapp.util.XMLHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+
 import static java.lang.System.exit;
 
 @SpringBootApplication
@@ -18,6 +20,11 @@ import static java.lang.System.exit;
 @ComponentScan
 @EntityScan("group.depapp")
 public class DepappApplication implements CommandLineRunner {
+
+    @Autowired
+    Synchronizer synchronizer;
+    @Autowired
+    XMLHandler xmlHandler;
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(DepappApplication.class);
@@ -29,9 +36,21 @@ public class DepappApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-
-//        Synchronizer.synchronize();
-//        XMLHandler.create();
+        if (args.length > 0) {
+            String command = args[0];
+            switch (command) {
+                case "create":
+                    xmlHandler.create();
+                    break;
+                case "sync":
+                    synchronizer.synchronize();
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            System.out.println("Не введена команда");
+        }
 
         exit(0);
     }
