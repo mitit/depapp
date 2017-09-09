9bfdbd2ed3e9eca1348b4d8192ac74ae419bc415
mikeqwe <mikee.qwe@gmail.com>
2017-09-09 14:13:32 +0300

    file schema.db

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE public.department
+(
+    id SERIAL PRIMARY KEY NOT NULL,
+    dep_code VARCHAR(20) NOT NULL,
+    dep_job VARCHAR(100) NOT NULL,
+    description VARCHAR(255)
+);
+
+CREATE UNIQUE INDEX department_id_uindex ON public.department (id);
